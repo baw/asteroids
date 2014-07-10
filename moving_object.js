492f5fa8f852b78e3eb532faaf73e83ead2f9cc8
Brian <iamamthwiz@gmail.com>
2014-07-10 01:09:12 -0700

    add some methods to moving object

--- moving_object.js
+++ moving_object.js
@@ -1,6 +1,44 @@
 (function (root) {
     var Asteroids = root.Asteroids = (root.Asteroids || {});
-    var MovingObject = Asteroids.MovingObject = function () {
+    var MovingObject = Asteroids.MovingObject = function (options) {
+        this.posX = options.pos[0];
+        this.posY = options.pos[1];
         
+        this.speed = options.speed ? options.speed : 0;
+        this.heading = options.heading ? options.heading : 0;
+        this.radius = options.radius ? options.radius : "10";
+        this.color = options.color ? options.color : "#FFF";
+    };
+    
+    MovingObject.draw = function (ctx) {
+        ctx.strokeStyle = this.color;
+        ctx.beginPath();
+        
+        var centerX = this.posX - (this.radius / 2);
+        var centerY = this.posY - (this.radius / 2);
+        
+        ctx.arc(
+            centerX,
+            centerY,
+            this.radius,
+            0,
+            2 * Math.PI,
+            false
+        );
+        
+        ctx.stroke();
+    };
+    
+    MovingObject.isCollidedWith = function (otherObject) {
+        var distance = Math.sqrt(
+            (Math.pow(this.posX - otherObject.posX, 2)) +
+            (Math.pow(this.posY - otherObject.posY, 2))
+        );
+        
+        if (distance < (this.radius + otherObject.radius)) {
+            return true;
+        } else {
+            return false;
+        }
     };
 })(this);
