1ca55002bad5609ef687c8d84f173847fe6c6363
Brian <iamamthwiz@gmail.com>
2014-07-12 18:28:03 -0700

    Add asteroid and randomAsteroid

--- asteroid.js
+++ asteroid.js
@@ -1,6 +1,28 @@
 (function (root) {
     var Asteroids = root.Asteroids = (root.Asteroids || {});
-    var Asteroid = Asteroids.Asteroid = function () {
+    var Asteroid = Asteroids.Asteroid = function (options) {
+        options.color = Asteroid.COLOR;
+        options.radius = Asteroid.RADIUS;
         
+        Asteroids.MovingObject.call(this, options);
+    };
+    Asteroid.COLOR = "#000";
+    Asteroid.RADIUS = 15;
+    Asteroid.inherits(Asteroids.MovingObject);
+    
+    Asteroid.randomAsteriod = function (dimX, dimY) {
+        var options = {
+            pos: [],
+            speed: 0,
+            heading: 0
+        }
+        
+        options.pos[0] = Asteroids.randomNumber(0, dimX);
+        options.pos[1] = Asteroids.randomNumber(0, dimY);
+        
+        options.speed = Asteroids.randomNumber(1, 4);
+        options.heading = Asteroids.randomNumber(0, 360);
+        
+        return new Asteroid(options);
     };
 })(this);
