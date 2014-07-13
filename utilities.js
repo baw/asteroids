8de27a2c5c6d999d84b4904515b52bb138a71e8c
Brian <iamamthwiz@gmail.com>
2014-07-12 18:30:20 -0700

    add randomNumber function

--- utilities.js
+++ utilities.js
@@ -6,4 +6,8 @@
         Surrogate.prototype = Parent.prototype;
         this.prototype = new Surrogate();
     };
+    
+    Asteroids.randomNumber = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
 })(this);
