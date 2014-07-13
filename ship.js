94f1f012cef4d9cbbf3cf5de9938aaa5deae9155
Brian <iamamthwiz@gmail.com>
2014-07-13 01:10:10 -0700

    Add power and turn methods to ship

--- ship.js
+++ ship.js
@@ -1,6 +1,32 @@
 (function (root) {
     var Asteroids = root.Asteroids = (root.Asteroids || {});
-    var Ship = Asteroids.Ship = function () {
+    var Ship = Asteroids.Ship = function (options) {
+        options.color = Ship.COLOR;
+        options.radius = Ship.RADIUS;
         
+        Asteroids.MovingObject.call(this, options);
+    };
+    Ship.RADIUS = 5;
+    Ship.COLOR = "#00F";
+    Ship.MAX_SPEED = 10;
+    Ship.inherits(Asteroids.MovingObject);
+    
+    Ship.prototype.power = function (impulse) {
+        if (this.speed >= 1 && this.speed < Ship.MAX_SPEED) {
+            this.speed += impulse;
+        } else if (this.speed < 1 && impulse > 0 ) {
+            this.speed += impulse;
+        } else if (this.speed >= Ship.MAX_SPEED && impulse < 0) {
+            this.speed += impulse;
+        }
+    };
+    
+    Ship.prototype.turn = function (turnAmmount) {
+        this.heading += turnAmmount;
+        if (this.heading < 0) {
+            this.heading += 360;
+        } else {
+            this.heading = this.heading % 360 ;
+        }
     };
 })(this);
